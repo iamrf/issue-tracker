8a420aafa31780c39ee689d36ba1107d899a0f7e
iamrf <rf.fa17@gmail.com>
2024-01-07 14:18:28 +0330

    Build a post api for creating issues, mongodb mongoose initialized

---
+++ app/api/issues/route.ts
@@ -0,0 +1,29 @@
+import dbConnect from "@/app/lib/dbConnect";
+import Issue, { Issues } from "@/app/models/Issue";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import _ from "lodash";
+
+const createIssueSchema = z.object({
+    title: z.string().min(1).max(255),
+    description: z.string(),
+});
+
+export async function POST(request: NextRequest, response: NextResponse) {
+    const senderIp = request.headers.get("x-forwarded-for");
+    const body = await request.json();
+    await dbConnect();
+    const validation = createIssueSchema.safeParse(body);
+    if (!validation.success) return NextResponse.json(validation.error.errors, { status: 400 });
+    try {
+        const newIssue = new Issue<Issues>({
+            title: body.title,
+            description: body.description,
+            status: body.status,
+        });
+        await newIssue.save();
+        return NextResponse.json(newIssue, { status: 201 });
+    } catch (ex) {
+        return NextResponse.json(ex, { status: 400 });
+    }
+}
---
+++ app/models/Issue.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+export interface Issues {
+    title: string;
+    description: string;
+    status: string;
+}
+
+/* IssueSchema will correspond to a collection in your MongoDB database. */
+const IssueSchema = new mongoose.Schema<Issues>(
+    {
+        title: {
+            type: String,
+            required: [true, "Please provide a title for this Issue."],
+            unique: true,
+            minlength: [1, "Name cannot be less than 1 characters"],
+            maxlength: [255, "Name cannot be more than 255 characters"],
+        },
+        description: {
+            type: String,
+            required: [true, "Please provide description for this Issue."],
+            minlength: [1, "Name cannot be less than 1 characters"],
+        },
+        status: {
+            type: String,
+            enum: ["OPEN", "CLOSED", "IN_PROGRESS"],
+            default: "OPEN",
+        },
+    },
+    { timestamps: true }
+);
+
+export default mongoose.models.Issue || mongoose.model<Issues>("Issue", IssueSchema);
