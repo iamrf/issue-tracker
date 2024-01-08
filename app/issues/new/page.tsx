b4eda706c305cb27c59d00f8077d668834c4eddb
iamrf <rf.fa17@gmail.com>
2024-01-08 18:36:32 +0330

    add spinner comp

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchemas';
 import { z } from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
 
 // interface IssueForm {
 //     title: string
@@ -21,6 +22,7 @@ const NewIssuePage = () => {
     const router = useRouter()
     const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({ resolver: zodResolver(createIssueSchema) })
     const [error, setError] = useState('')
+    const [isSubmitting, setSubmitting] = useState(false)
 
     return (
         <div className='max-w-xl'>
@@ -34,9 +36,11 @@ const NewIssuePage = () => {
 
             <form onSubmit={handleSubmit(async (data) => {
                 try {
+                    setSubmitting(true)
                     await axios.post('/api/issues', data)
                     router.push('/issues')
                 } catch (error) {
+                    setSubmitting(false)
                     setError('an error occured')
                 }
             })} className='space-y-4'>
@@ -50,7 +54,7 @@ const NewIssuePage = () => {
                     render={({ field }) => <SimpleMDE placeholder="write description" {...field} />}
                 />
                 <ErrorMessage>{errors.description?.message}</ErrorMessage>
-                <Button>Submit New Issue</Button>
+                <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner />}</Button>
             </form>
         </div>
     )
