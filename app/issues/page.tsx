e082df4b82791d69e66ef523368605319d0fe32a
iamrf <rf.fa17@gmail.com>
2024-01-08 20:17:28 +0330

    show issues table

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,55 @@
-import { Button } from '@radix-ui/themes'
+import { Button, Table, TableColumnHeaderCell } from '@radix-ui/themes'
 import Link from 'next/link'
 import React from 'react'
+import dbConnect from '../lib/dbConnect'
+import Issue from '../models/Issue'
 
-const IssuesPage = () => {
+const IssuesPage = async () => {
+    await dbConnect();
+    const issues = await Issue.find();
     return (
-        <div><Button><Link href="/issues/new">New Issue</Link></Button></div>
+        <div>
+            <div className='mb-4'>
+                <Button><Link href="/issues/new">New Issue</Link></Button>
+            </div>
+            <Table.Root variant="surface">
+                <Table.Header>
+                    <Table.Row>
+                        <TableColumnHeaderCell>
+                            Issue
+                        </TableColumnHeaderCell>
+                        <TableColumnHeaderCell className='hidden md:table-cell'>
+                            Status
+                        </TableColumnHeaderCell>
+                        <TableColumnHeaderCell>
+                            Description
+                        </TableColumnHeaderCell>
+                        <TableColumnHeaderCell className='hidden md:table-cell'>
+                            Created
+                        </TableColumnHeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {issues.map(issue => {
+                        return <Table.Row key={issue._id}>
+                            <Table.RowHeaderCell>
+                                {issue.title}
+                                <div className='block md:hidden text-xs'>{issue.status}</div>
+                            </Table.RowHeaderCell>
+                            <Table.RowHeaderCell className='hidden md:table-cell'>
+                                {issue.status}
+                            </Table.RowHeaderCell>
+                            <Table.RowHeaderCell>
+                                {issue.description}
+                            </Table.RowHeaderCell>
+                            <Table.RowHeaderCell className='hidden md:table-cell'>
+                                {issue.createdAt.toDateString()}
+                            </Table.RowHeaderCell>
+                        </Table.Row>
+                    })}
+                </Table.Body>
+            </Table.Root>
+        </div>
     )
 }
 
