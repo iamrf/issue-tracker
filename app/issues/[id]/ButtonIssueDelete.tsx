c73f61b0ff3bd847acbc80d3eeaef84f0f56b341
iamrf <rf.fa17@gmail.com>
2024-01-10 21:33:22 +0330

    Delete confirmation dialog box created

--- app/issues/[id]/ButtonIssueDelete.tsx
+++ app/issues/[id]/ButtonIssueDelete.tsx
@@ -1,13 +1,29 @@
-import { Button } from '@radix-ui/themes'
-import Link from 'next/link'
+'use client'
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import { PiTrash } from 'react-icons/pi'
 
-const ButtonIssueDelete = ({ issueId }: { issueId: string }) => {
+const ButtonIssueDelete = ({ issueId, issueTitle }: { issueId: string, issueTitle: string }) => {
     return (
-        <Button color='red'>
-            <PiTrash className="text-lg" />
-            &nbsp;Delete Issue
-        </Button>
+        <AlertDialog.Root>
+            <AlertDialog.Trigger>
+                <Button color='red'>
+                    <PiTrash className="text-lg" />
+                    &nbsp;Delete Issue
+                </Button>
+            </AlertDialog.Trigger>
+            <AlertDialog.Content>
+                <AlertDialog.Title>Confirm Delete {issueTitle}</AlertDialog.Title>
+                <AlertDialog.Description>Are you sure?</AlertDialog.Description>
+                <Flex mt="4" gap='4'>
+                    <AlertDialog.Cancel>
+                        <Button color='gray' variant='soft'>Cancel</Button>
+                    </AlertDialog.Cancel>
+                    <AlertDialog.Action>
+                        <Button color='red'>Delete</Button>
+                    </AlertDialog.Action>
+                </Flex>
+            </AlertDialog.Content>
+        </AlertDialog.Root>
     )
 }
 
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -32,7 +32,7 @@ const IssueDetailPage = async ({ params }: Props) => {
             <Box>
                 <Flex direction="column" gap="4">
                     <ButtonIssueEdit issueId={issue.id} />
-                    <ButtonIssueDelete issueId={issue.id} />
+                    <ButtonIssueDelete issueId={issue.id} issueTitle={issue.title} />
                 </Flex>
             </Box>
         </Grid>
