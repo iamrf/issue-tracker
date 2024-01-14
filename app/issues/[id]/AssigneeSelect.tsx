0ca78cca17b60994d4c5e32f7b641c1acfe78b56
iamrf <rf.fa17@gmail.com>
2024-01-14 15:29:55 +0330

    fetch data with react-query

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,25 +1,28 @@
 'use client'
 import { Users } from '@/app/models/User'
 import { Select } from '@radix-ui/themes'
+import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import { Skeleton } from '@/app/components'
 
 const AssigneeSelect = () => {
-    const [users, setUsers] = useState<Users[]>([])
-    useEffect(() => {
-        const fetchUsers = async () => {
-            const { data } = await axios.get<Users[]>('/api/users')
-            setUsers(data)
-        }
-        fetchUsers()
-    }, [])
+    const { data: users, error, isLoading } = useQuery<Users[]>({
+        queryKey: ['users'],
+        queryFn: () => axios.get('/api/users').then(res => res.data),
+        staleTime: 60 * 1000,
+        retry: 3
+    })
+
+    if (isLoading) return <Skeleton height="2rem" />
+    if (error) return null
+
     return (
         <Select.Root>
             <Select.Trigger placeholder='Assign ...' />
             <Select.Content>
                 <Select.Group>
                     <Select.Label>Suggestions</Select.Label>
-                    {users.map(user => (
+                    {users?.map(user => (
                         <Select.Item key={user.email} value={user.email}>{user.name}</Select.Item>
                     ))}
                 </Select.Group>
