d3032366a6ef66bd7f252ca4abe72bd3987df8cf
iamrf <rf.fa17@gmail.com>
2024-01-17 18:33:23 +0330

    show toast notifs

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -4,6 +4,7 @@ import { Issue, User } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
+import toast, { Toaster } from "react-hot-toast"
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     const { data: users, error, isLoading } = useQuery<User[]>({
@@ -17,20 +18,29 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     if (error) return null
 
     return (
-        <Select.Root defaultValue={issue.userId || "remove"} onValueChange={(userId) => {
-            axios.patch(`/api/issues/${issue.id}`, userId === "remove" ? { userId: null } : { userId: userId })
-        }}>
-            <Select.Trigger placeholder='Assign ...' />
-            <Select.Content>
-                <Select.Group>
-                    <Select.Label>Suggestions</Select.Label>
-                    <Select.Item value="remove">- UnAssigned</Select.Item>
-                    {users?.map(user => (
-                        <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
-                    ))}
-                </Select.Group>
-            </Select.Content>
-        </Select.Root>
+        <>
+            <Select.Root defaultValue={issue.userId || "remove"} onValueChange={(userId) => {
+                axios.patch(`/api/issues/${issue.id}`, userId === "remove" ? { userId: null } : { userId: userId })
+                    .then(() => {
+                        toast.success('success added ')
+                    })
+                    .catch(() => {
+                        toast.error('Changes could not be applied')
+                    })
+            }}>
+                <Select.Trigger placeholder='Assign ...' />
+                <Select.Content>
+                    <Select.Group>
+                        <Select.Label>Suggestions</Select.Label>
+                        <Select.Item value="remove">- UnAssigned</Select.Item>
+                        {users?.map(user => (
+                            <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
+                        ))}
+                    </Select.Group>
+                </Select.Content>
+            </Select.Root>
+            <Toaster />
+        </>
     )
 }
 
